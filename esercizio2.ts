b557beab8e8fd7c99ecdb31ea6c063cc107ecbc5
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-11-20 18:50:08 +0100

    2

--- esercizio2.ts
+++ esercizio2.ts
@@ -1,1 +1,27 @@
-let notes: (number | string)[] = ["Get Food", 23, "Call the previous number when betting"];
+/**
+ * START: Follow the instructions below.
+ */
+
+// Add type annotations to each of these variables.
+
+let country: string = "Italy";
+
+let year: any = undefined;
+
+let averageTemperature: number = 23.6;
+
+let visited: boolean = true;
+
+let currency: any = null;
+
+// Change the type annotations on these variables so they are correct.
+
+let population: number = 47_450_795;
+
+let isSummer: boolean = false;
+
+let languages: string = "Spanish, English";
+
+// ----
+
+export {};
